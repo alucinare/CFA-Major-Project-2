491a29aace4887c815f41656de5468b519fe6a8a
John S <alucinare@gmail.com>
2017-04-22 21:54:16 +1000

    Add color to dis/agree

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -18,9 +18,11 @@ jQuery(document).on('turbolinks:load', function() {
 
       renderMessage: function(data) {
 
+        var discuss = ((data.connect == "Agree") ? data.connect.fontcolor("green") : data.connect.fontcolor("red"));
+
         // discuss = changeTextColour(data);
 
-        return "<p> <b>" + data.user + "</b>" + "(" + data.connect + ")" + ": " + data.message + "</p>";
+        return "<p> <b>" + data.user + "</b>" + "(" + discuss + ")" + ": " + data.message + "</p>";
       }
     });
   } //sitepoint
@@ -33,47 +35,3 @@ jQuery(document).on('turbolinks:load', function() {
   //     return data.connect.fontcolor("red");
   //   }
   // }
-
-// discuss = data.connect
-// if (discuss = "Agree") {
-//   discuss = discuss.fontcolor("green");
-// } else {
-//   discuss = discuss.fontcolor("red");
-// }
-// jQuery(document).on('turbolinks:load', function() {
-//   var messages, messages_to_bottom;
-//   messages = $('#messages');
-//   if ($('#messages').length > 0) {
-//     messages_to_bottom = function() {
-//       return messages.scrollTop(messages.prop("scrollHeight"));
-//     };
-//     messages_to_bottom();
-//     App.global_chat = App.cable.subscriptions.create({
-//       channel: "ChatRoomsChannel",
-//       chat_room_id: messages.data('chat-room-id')
-//     }, {
-//       received: function(data) {
-//         messages.append(data['message']);
-//         return messages_to_bottom();
-//       },
-//
-//       send_message: function(message, chat_room_id) {
-//         return this.perform('send_message', {
-//           message: message,
-//           chat_room_id: chat_room_id
-//         });
-//       }
-//     });
-//     return $('#new_message').submit(function(e) {
-//       var $this, textarea;
-//       $this = $(this);
-//       textarea = $this.find('#message_body');
-//       if ($.trim(textarea.val()).length > 1) {
-//         App.global_chat.send_message(textarea.val(), messages.data('chat-room-id'));
-//         textarea.val('');
-//       }
-//       e.preventDefault();
-//       return false;
-//     });
-//   }
-// });
